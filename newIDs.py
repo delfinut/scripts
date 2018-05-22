4363b362897a33737524eff9fe01c1dce351e8f1
delfinut <dv255@cam.ac.uk>
2018-05-22 12:15:22 +0100

    annotated script with description, usage, and things to do to improve it

--- newIDs.py
+++ newIDs.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 
+#######
+
+# Description: 
+# Script to replace current fasta headers with new headers by looking up and matching from a tab delimited file 
+# called "newnames.txt" which has current headers in the first column and new headers in the second column 
+# outputs a fasta file with new headers whose name has "-newIDs.fa" appended to the original input file name.
+
+
+# Usage:
+# newIDs.py [fastafile in which headers need to be replaced]
+
+
+# TO DO to improve script
+
+# argparse to be able to input fasta file and names file with -f and -n arguments 
+# (I get confused which goes first when using just sys.argv so the name of the tab delimited file is fixed for the moment)
+
+# test script
+
+# make it a function that can be used as part of other scripts
+
+#######
+
 import sys 
 import os
 from Bio import SeqIO
@@ -26,11 +49,11 @@ with open(infile) as original, open(outfile, 'w') as corrected:
     records = SeqIO.parse(infile, 'fasta')
     for record in records:         
         if record.id in pairdict:
-            #print("Old ID is", record.id)
+            #print("Old ID is", record.id) #Testing
             record.id = pairdict.get(record.id)
-            #print("New ID is", record.id)
+            #print("New ID is", record.id) #Testing
             record.description = record.id 
-            # makes it not provide old and new names in header, just new
+            # above makes it not provide old and new names in header, just new
         SeqIO.write(record, corrected, 'fasta')
 
 print("fini!")
